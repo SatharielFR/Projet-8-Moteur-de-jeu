20a40ca3d085607c286982e3227ee684c639b2ac
Sathariel <leochabert@free.fr>
2022-03-08 14:54:51 +0100

    Add, c++ Project

---
+++ Projet 8 - Groupe 1/Engine/Engine.cpp
@@ -0,0 +1,10 @@
+// Engine.cpp : Définit les fonctions de la bibliothèque statique.
+//
+
+#include "pch.h"
+#include "framework.h"
+
+// TODO: Il s'agit d'un exemple de fonction de bibliothèque
+void fnEngine()
+{
+}
