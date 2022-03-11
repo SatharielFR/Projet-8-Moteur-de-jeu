d1337f898235f62ef07fabb90b375322887a99d7
Sathariel <leochabert@free.fr>
2022-03-11 10:33:02 +0100

    Mod, CleanUp Code (again)

--- Projet 8 - Groupe 1/GameEngine/framework.h
+++ Projet 8 - Groupe 1/GameEngine/framework.h
@@ -1,11 +1,5 @@
-// header.h : fichier Include pour les fichiers Include système standard,
-// ou les fichiers Include spécifiques aux projets
-//
-
 #pragma once
 
 #include "targetver.h"
-#define WIN32_LEAN_AND_MEAN             // Exclure les en-têtes Windows rarement utilisés
-
 //Link to our Engine
 #include "../Engine/framework.h"
