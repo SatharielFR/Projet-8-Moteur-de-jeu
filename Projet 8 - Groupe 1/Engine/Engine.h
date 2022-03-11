cf707848d3b7ebb10c94ce862fb05e6ac2de50c1
Sathariel <leochabert@free.fr>
2022-03-11 10:05:37 +0100

    Mod, Transform

--- Projet 8 - Groupe 1/Engine/Engine.h
+++ Projet 8 - Groupe 1/Engine/Engine.h
@@ -1,3 +1,23 @@
 #pragma once
 
-#include "Transform.h"
+//TODO : Supprimer ca quand tout aurras été fait dans l engine 
+// include the basic windows header files and the Direct3D header file
+#include <d3d9.h>
+#include <d3dx9.h>
+
+// include the Direct3D Library file
+#pragma comment (lib, "d3d9.lib")
+#pragma comment (lib, "d3dx9.lib")
+
+
+// global declarations
+LPDIRECT3D9 d3d;    // the pointer to our Direct3D interface
+LPDIRECT3DDEVICE9 d3ddev;    // the pointer to the device class
+LPDIRECT3DVERTEXBUFFER9 v_buffer = NULL;    // the pointer to the vertex buffer
+LPDIRECT3DINDEXBUFFER9 i_buffer = NULL;     // the pointer to the index buffer
+
+// Fichiers d'en-tête C RunTime
+#include <stdlib.h>
+#include <malloc.h>
+#include <memory.h>
+#include <tchar.h>
--- Projet 8 - Groupe 1/Engine/Transform.h
+++ Projet 8 - Groupe 1/Engine/Transform.h
@@ -1,38 +1,41 @@
 #pragma once
 
+#include "Engine.h"
 
+//#include <D3DX10math.h>
+//// include the Direct3D Library file
+//#pragma comment (lib, "D3DX10math.lib")
 
 class Transform
 {
 	public:
 		#pragma region  Variables
-			//VECTOR3 vSca;
-			//VECTOR3 vDir;
-			//VECTOR3 vRight;
-			//VECTOR3 vUp;
-			//QUATERNION qRot;
-			//MATRIX mRot;
-			//VECTOR3 vPos;
-			//MATRIX matrix;
+			D3DMATRIX			m_vec3Sca;
+			D3DVECTOR			m_vec3Dir;
+			D3DVECTOR			m_vec3Right;
+			D3DVECTOR			m_veec3Up;
+			D3DXQUATERNION		m_quatRot;
+			D3DXMATRIX			m_matrixRot;
+			D3DVECTOR			m_vec3Pos;
+			D3DXMATRIX			m_matrix;
 		#pragma endregion
 
 		#pragma region PublicFunction
 			Transform();
-			void Test();
-			//void Identity();
-			//void FromMatrix(MATRIX* pMat);
-			//void UpdateRotationFromVectors();
-			//void UpdateRotationFromQuaternion();
-			//void UpdateRotationFromMatrix();
-			//void UpdateMatrix();
-			//void Rotate(float yaw, float pitch, float roll);
-			//void RotateYaw(float angle);
-			//void RotatePitch(float angle);
-			//void RotateRoll(float angle);
-			//void RotateWorld(MATRIX* pMatrix);
-			//void RotateWorldX(float angle);
-			//void RotateWorldY(float angle);
-			//void RotateWorldZ(float angle);
+			void Identity();
+			void FromMatrix(D3DMATRIX* pMat);
+			void UpdateRotationFromVectors();
+			void UpdateRotationFromQuaternion();
+			void UpdateRotationFromMatrix();
+			void UpdateMatrix();
+			void Rotate(float yaw, float pitch, float roll);
+			void RotateYaw(float angle);
+			void RotatePitch(float angle);
+			void RotateRoll(float angle);
+			void RotateWorld(D3DMATRIX* pMatrix);
+			void RotateWorldX(float angle);
+			void RotateWorldY(float angle);
+			void RotateWorldZ(float angle);
 		#pragma endregion 
 };
 
