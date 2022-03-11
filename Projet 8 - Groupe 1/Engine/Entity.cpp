dcd8d68d5bb5e94adc9542ddcbb64f178dc2e6f5
DumezRodrigue <74072465+DumezRodrigue@users.noreply.github.com>
2022-03-11 15:32:24 +0100

    Simple Function Entity

--- Projet 8 - Groupe 1/Engine/Entity.cpp
+++ Projet 8 - Groupe 1/Engine/Entity.cpp
@@ -1,2 +1,54 @@
 #include "pch.h"
 #include "Entity.h"
+
+void Entity::Begin()
+{
+
+}
+
+void Entity::Update()
+{
+
+}
+
+void Entity::SetParent(Entity* newParentEntity)
+{
+	if (_entityParent != nullptr) 
+	{ 
+		return; 
+	}
+	else
+	{
+		_entityParent = newParentEntity;
+	}
+}
+
+void Entity::AddChild(Entity* newChildToAdd)
+{
+	if (newChildToAdd == nullptr) { return; }
+
+	for (auto i = _lstEntityChild.begin(); i != _lstEntityChild.end(); i++)
+	{
+		if (*i != newChildToAdd)
+		{
+			_lstEntityChild.push_back(newChildToAdd);
+		}
+		else
+		{
+			return;
+		}
+	}
+}
+
+void Entity::RemoveChild(Entity* childToRem)
+{
+	if (childToRem == nullptr) { return; }
+
+	for (auto i = _lstEntityChild.begin(); i != _lstEntityChild.end(); i++)
+	{
+		if(*i == childToRem)
+		{
+			_lstEntityChild.erase(i);
+		}
+	}
+}
