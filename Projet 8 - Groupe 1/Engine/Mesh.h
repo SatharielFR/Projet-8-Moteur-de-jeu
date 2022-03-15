ebc2f311b733554087a8f09d329e774d21bfa802
Nayie <tom.louet.tl@gmail.com>
2022-03-15 15:22:47 +0100

    fleshed out mesh class

--- Projet 8 - Groupe 1/Engine/Mesh.h
+++ Projet 8 - Groupe 1/Engine/Mesh.h
@@ -5,13 +5,36 @@ class Mesh
 {
 public:
 
+#pragma region variables
     DWORD _numMaterials;
     LPD3DXMESH _mesh;
     LPDIRECT3DTEXTURE9* _texture;
     D3DMATERIAL9* _material;
+#pragma endregion
 
+#pragma region Constructeur
     Mesh(DWORD materials, LPD3DXMESH mesh, LPDIRECT3DTEXTURE9* texture, D3DMATERIAL9* material);
-    void LoadMesh(Mesh* mesh, LPCTSTR File);
+#pragma endregion
+
+#pragma region methods
+    void LoadMesh(Mesh* mesh, LPCTSTR file, LPDIRECT3DDEVICE9 device);
+#pragma endregion
+
+#pragma region accessors
+    DWORD GetNumMaterials();
+    LPD3DXMESH GetMesh();
+    LPDIRECT3DTEXTURE9* GetTexture();
+    D3DMATERIAL9* GetMaterial();
+#pragma endregion
+
+#pragma region mutators
+    void SetNumMaterials(DWORD numMaterials);
+    void SetMesh(LPD3DXMESH mesh);
+    void SetTexture(LPDIRECT3DTEXTURE9* texture);
+    void SetMaterial(D3DMATERIAL9* material);
+#pragma endregion
+
+
 
 };
 
