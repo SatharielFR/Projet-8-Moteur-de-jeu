43ac0d40b67d4ad87bd76893545b9039b6712db5
Nayie <tom.louet.tl@gmail.com>
2022-03-18 09:36:29 +0100

    updated mesh methods

--- Projet 8 - Groupe 1/Engine/Mesh.cpp
+++ Projet 8 - Groupe 1/Engine/Mesh.cpp
@@ -10,32 +10,36 @@ Mesh::Mesh(DWORD materials, LPD3DXMESH mesh, LPDIRECT3DTEXTURE9* texture, D3DMAT
     _material = material;
 }
 
+/// <summary>
+/// this has to be tested, it probably does not work properly
+/// this method should only be called in scene as we do not have access to the device (d3ddev) in this class
+/// </summary>
+/// <param name="mesh">mesh of the Mesh object</param>
+/// <param name="file">filepath to .x file</param>
+/// <param name="device">device to draw on PASSED BY SCENE</param>
 void Mesh::LoadMesh(Mesh* mesh, LPCTSTR file, LPDIRECT3DDEVICE9 device)
 {
-    // on passe le device par la scène et c'est elle qui va draw
-    //LPD3DXBUFFER bufMaterial;
-    //D3DXLoadMeshFromXW(file, D3DXMESH_SYSTEMMEM, d3ddev, NULL, &bufMaterial, NULL,
-    //    &mesh->_numMaterials, &mesh->_mesh);
-    //OutputDebugString(L"LOAD MESH \n");
+     //on passe le device par la scène et c'est elle qui va draw
+    LPD3DXBUFFER bufMaterial;
+    D3DXLoadMeshFromXW(file, D3DXMESH_SYSTEMMEM, device, NULL, &bufMaterial, NULL, &mesh->_numMaterials, &mesh->_mesh);
 
-    //D3DXMATERIAL* tempMaterials = (D3DXMATERIAL*)bufMaterial->GetBufferPointer();
-    //OutputDebugString(L"GET BUFFER\n");
+    D3DXMATERIAL* tempMaterials = (D3DXMATERIAL*)bufMaterial->GetBufferPointer();
 
-    //mesh->_material = new D3DMATERIAL9[mesh->numMaterials];
-    //mesh->_texture = new LPDIRECT3DTEXTURE9[mesh->numMaterials];
+    mesh->_material = new D3DMATERIAL9[mesh->_numMaterials];
+    mesh->_texture = new LPDIRECT3DTEXTURE9[mesh->_numMaterials];
 
-    //for (DWORD index = 0; index < mesh->_numMaterials; index++)
-    //{
-    // this should be updated after renaming, probably broke
-    //    mesh->Material[index] = tempMaterials[index].MatD3D;
-    //    mesh->Material[index].Ambient = mesh->Material[index].Diffuse;
+    for (DWORD index = 0; index < mesh->_numMaterials; index++)
+    {
+     //this should be updated after renaming, probably broke
+        mesh->_material[index] = tempMaterials[index].MatD3D;
+        mesh->_material[index].Ambient = mesh->_material[index].Diffuse;
 
-    //    // if there is a texture to load, load it 
-    //    if (FAILED(D3DXCreateTextureFromFileA(d3ddev,
-    //        tempMaterials[index].pTextureFilename,
-    //        &mesh->Texture[index])))
-    //        mesh->Texture[index] = NULL;    // if there is no texture, set the texture to NULL 
-    //}
+        // if there is a texture to load, load it 
+        if (FAILED(D3DXCreateTextureFromFileA(device,
+            tempMaterials[index].pTextureFilename,
+            &mesh->_texture[index])))
+            mesh->_texture[index] = NULL;    // if there is no texture, set the texture to NULL 
+    }
 
     return;
 }
@@ -72,8 +76,5 @@ void Mesh::LoadMesh(Mesh* mesh, LPCTSTR file, LPDIRECT3DDEVICE9 device)
     }
 #pragma endregion
 
-
-
-    // NEED MUTATORS ???
     //https://www.unknowncheats.me/forum/d3d-tutorials-and-source/102186-d3d9-mesh-directx-format-loading-class.html
     //we are using this => https://stackoverflow.com/questions/996879/loading-mesh-from-x-file-with-direct-x-9-in-c
