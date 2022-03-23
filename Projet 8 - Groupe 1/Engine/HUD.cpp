d47f3014e4b1b11ed3c7bf731f2f7b9969207615
Nayie <tom.louet.tl@gmail.com>
2022-03-23 18:13:03 +0100

    HUD first tests / drawing text and int / creating dynamic rect for drawing based on blackboxw

--- Projet 8 - Groupe 1/Engine/HUD.cpp
+++ Projet 8 - Groupe 1/Engine/HUD.cpp
@@ -6,12 +6,14 @@
 HUD::HUD() {
 	g_lpTexture = NULL;
 	g_lpVBuffer = NULL;
+	_rec = RECT();
 }
 
 
 
 		//UNTESTED AND PROBABLY INCOMPELTE
 void HUD::InitializeHUD(LPDIRECT3DDEVICE9 device) {
+	//REMOVING THIS IF BREAKS EVERYTHING I DONT KNOW WHY
 	if (FAILED(device->CreateVertexBuffer(sizeof(TLVERTEX) * 4, 0, FVF_TLVERTEX, D3DPOOL_DEFAULT, &g_lpVBuffer, NULL))) 
 	{ 
 		//Somethings gone wrong! handle an error here and bail out gracefully
@@ -22,10 +24,10 @@ void HUD::InitializeHUD(LPDIRECT3DDEVICE9 device) {
 		g_lpVBuffer->Lock(0, 0, reinterpret_cast<void**>(&pData), 0);
 
 		//creating quad coords
-		float low_x = 200.0f;
-		float low_y = 0.0f;
-		float high_x = 700.0f;
-		float high_y = 100.0f;
+		float low_x = SCREEN_WIDTH * 0.25;
+		float high_x = SCREEN_WIDTH * 0.75;
+		float low_y = 0;
+		float high_y = SCREEN_WIDTH * 0.05;
 		//
 
 
@@ -42,6 +44,20 @@ void HUD::InitializeHUD(LPDIRECT3DDEVICE9 device) {
 		pData[3].t = D3DXVECTOR2(1.0f, 1.0f);
 		g_lpVBuffer->Unlock();
 	//
+
+		//create font
+		CreateHUDFont(&device);
+
+		//create margins for better display
+		int lowXToGive, highXToGive, lowYToGive, highYToGive;
+		lowXToGive = (int)low_x;
+		highXToGive = (int)high_x;
+		lowYToGive = (int)low_y;
+		highYToGive = (int)high_y;
+		//create Rect to display in
+		CreateRectForDisplay(lowXToGive, highXToGive, lowYToGive, highYToGive);
+
+
 }
 
 //UNTESTED AND PROBABLY INCOMPLETE
@@ -53,4 +69,84 @@ void HUD::UpdateHUD(LPDIRECT3DDEVICE9 device) {
 	// add images and text
 	//draw all images
 	//draw all texts
+}
+
+void HUD::DisplayText(string* strToDisplay, float fDuration) {
+	RECT newRec;
+	newRec.left = 20;
+	newRec.top = 20;
+	newRec.right = SCREEN_WIDTH;
+	newRec.bottom = SCREEN_HEIGHT;
+
+	//Create the text to display
+	string finalStrToLog = *strToDisplay;
+
+	//Display the font text using the rect and font
+	_wstrToLog = std::wstring(finalStrToLog.begin(), finalStrToLog.end());
+	const WCHAR* wcharTextToDisplay = _wstrToLog.c_str();
+	LPCWSTR textToDisplay = LPCWSTR(wcharTextToDisplay);
+	if (_Font)
+	{
+		_Font->DrawText(
+			NULL,
+			textToDisplay, // String to draw.
+			-1, // Null terminating string.
+			&newRec, // Rectangle to draw the string in.
+			DT_TOP | DT_LEFT, // Draw in top-left corner of rect.
+			_DefaultColor); // Black.
+	}
+}
+
+void HUD::CreateHUDFont(LPDIRECT3DDEVICE9* device) {
+	//Create font
+	D3DXFONT_DESC fontDesc;
+	ZeroMemory(&fontDesc, sizeof(D3DXFONT_DESC));
+	fontDesc.Height = 25;
+	fontDesc.Width = 12;
+	fontDesc.Weight = 500;
+	fontDesc.MipLevels = D3DX_DEFAULT;
+	fontDesc.Italic = false;
+	fontDesc.CharSet = 0;
+	fontDesc.OutputPrecision = 0;
+	fontDesc.Quality = 0;
+	fontDesc.PitchAndFamily = 0;
+	char* test = (char*)fontDesc.FaceName;
+	strcpy(test, "Times New Roman"); // font style
+	D3DXCreateFontIndirect(*device, &fontDesc, &_Font);
+}
+
+void HUD::DisplayInt(int intToDisplay, float fDuration) {
+
+	//Create the text to display
+	string finalStrToLog = to_string(intToDisplay);
+
+	//Display the font text using the rect and font
+	_wstrToLog = std::wstring(finalStrToLog.begin(), finalStrToLog.end());
+	const WCHAR* wcharTextToDisplay = _wstrToLog.c_str();
+	LPCWSTR textToDisplay = LPCWSTR(wcharTextToDisplay);
+	if (_Font)
+	{
+		_Font->DrawText(
+			NULL,
+			textToDisplay, // String to draw.
+			-1, // Null terminating string.
+			&_rec, // Rectangle to draw the string in.
+			DT_TOP | DT_LEFT, // Draw in top-left corner of rect.
+			_DefaultColor); // Black.
+	}
+}
+
+void HUD::CreateRectForDisplay(int leftIn, int rightIn, int topIn, int bottomIn) {
+	//Create Rectangle to draw the font
+	RECT  Rec;
+	Rec.left = leftIn;
+	Rec.top = topIn;
+	Rec.right = rightIn;
+	Rec.bottom = bottomIn;
+
+	_rec = Rec;
+}
+
+RECT HUD::getRect() {
+	return _rec;
 }
