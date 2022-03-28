b6aafcdd2d8d6a1bd50f26d95e70c396f11dd2b0
Sathariel <leochabert@free.fr>
2022-03-28 15:36:02 +0200

    Add, Crosshair

--- Projet 8 - Groupe 1/Engine/Sprite.h
+++ Projet 8 - Groupe 1/Engine/Sprite.h
@@ -1,6 +1,26 @@
 #pragma once
 
-class Sprite
+class Sprite : public HudElement
 {
+	public:
+		#pragma region PublicFunctions
+			Sprite();
+			~Sprite();
+			void Update();
+			void SetTexture(string strTextureName);
+			void DrawSprite();
+			void SetScale(float fScale);
+			void SetPosition(int x, int y);
+			void SetSpriteColor(D3DXCOLOR* color);
+		#pragma endregion
+
+	private :
+		#pragma region PrivateVariables
+			Transform _transform;
+			ID3DXSprite* _sprite;
+			D3DXCOLOR* _colorSprite;
+			LPDIRECT3DTEXTURE9 _text;
+		#pragma endregion
+
 };
 
